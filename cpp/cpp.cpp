14a0d720395cf2132ee94c2bef9a76390f1ea3e0
Rodz Amor <ramorSTEM2022@gmail.com>
2022-06-25 01:48:38 -0400

    qwer

--- cpp/cpp.cpp
+++ cpp/cpp.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello world!";
-  cout << " This " << "is " << "awesome!";
-  return 0;
+    cout << "Hello world!";
+    cout << " This " << "is " << "awesome!";
+    cout << "endl makes a line break" << endl;
+    cout << "\n also does the same";
+
+    int variable = 10;
+    int a,b;
+
+    int num;
+    cin >> num; // cin is used for user input
+    cin >> a >> b;
+    cout << num << a << b;
+
+
+    return 0;
 }
