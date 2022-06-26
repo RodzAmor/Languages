8f79793502d1b6ab0a7bc028924d3fb54c448ede
Rodz Amor <ramorSTEM2022@gmail.com>
2022-06-25 23:28:48 -0400

    html

--- cpp/cpp.cpp
+++ cpp/cpp.cpp
@@ -16,6 +16,19 @@ int main()
     cin >> a >> b;
     cout << num << a << b;
 
+    // auto keyword automatically deducts type of variable 
+    // must be initialized at its declaration
+    auto x = 41; // integer
+    auto pi = 3.14; // float
+    auto phrase = "phrase"; // string
 
+    x = 5;
+    int y = ++x; // prefix
+    // x is 6, y is 6
+    
+    x = 5;
+    y = x++;
+    // x is 6, y is 5
+    
     return 0;
 }
